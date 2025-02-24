f091f4af44ccc0386df5c3e481618e11a00e94ec
zcopycat <zvilsdorfa@outlook.com>
2025-02-24 22:30:23 +0000

    Added OrderingTCs page

---
+++ src/pages/OrderingTCs.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function OrderingTCs() {
+  return (
+    <div className="about-us">
+      <h1 className="cursive-heading">Ordering Terms and Conditions</h1>
+      <p>
+      We prepare all of our food in house - by hand, in our kitchens. 
+      Please be aware that all food may contain traces of allergens.
+      </p>
+      <p>
+      When selecting delivery time slots, please allow plenty of time for your order as we are at the mercy of traffic which can be unpredictable.
+      We will attempt delivery of your order only once. If no one is available to receive it, the order will be taken back to our bakery. 
+      You can still collect the food at the bakery, before closing time. 
+      </p>
+      <p>
+      If the food has been out for a long period of time, some items may no longer be safe for consumption. 
+      In such instances, we cannot provide a refund. Any uncollected orders will be donated to our charity partner at the end of day.
+      </p>
+      <p>
+      If you wish to add special instructions for our bakery or couriers, 
+      you can do so up until 11am 2 working days before by contacting us via our contact page.
+      </p>
+    </div>
+  );
+}
