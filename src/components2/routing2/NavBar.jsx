3d8dbc8b6103128fb588060880b7cc6602b3fb07
zcopycat <zvilsdorfa@outlook.com>
2025-03-06 21:32:31 +0000

    organise navbar files

--- src/components2/routing2/NavBar.jsx
+++ src/components2/routing2/NavBar.jsx
@@ -1,50 +1,28 @@
+// src/components/Navbar.js
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles2/Navbar.css";
+import "../../styles2/Navbar.css"; // We'll add styling for the navbar
 
-export default function NavBar() {
+const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          Bean & Ivy
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                Our Story
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Visit Us
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/checkout">
-                Basket
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav className="navbar">
+      <h1 className="navbar-cafe-name">Bean & Ivy Cafe</h1>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+      </ul>
     </nav>
   );
-}
+};
+
+export default Navbar;
