1437561f947c8eaf48787751c2a73363c33719f0
zcopycat <zvilsdorfa@outlook.com>
2025-03-06 21:35:16 +0000

    updated contact page

--- src/pages2/Contact.jsx
+++ src/pages2/Contact.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <h2>Contact Us</h2>
-      <p>Email: contact@beanandivy.com</p>
-      <p>Phone: +123 456 7890</p>
-      <p>Visit us at 123 Coffee Lane, Brewtown.</p>
+    <div className="contactContainer">
+      <div className="about-us">
+        <h2 className="cursive-heading">Contact Us</h2>
+        <p>Email: contact@beanandivy.com</p>
+        <p>Phone: +123 456 7890</p>
+        <p>Visit us at 123 Coffee Lane, Brewtown</p>
+      </div>
     </div>
   );
 };
