09116622cf943a9fea09194bffea7e54c96cc093
BRITISH TRADERS <minahazel2023@yahoo.com>
2025-03-09 23:31:42 +0000

    Contact.jsx: updated details

--- src/pages2/Contact.jsx
+++ src/pages2/Contact.jsx
@@ -5,11 +5,15 @@ const Contact = () => {
     <div className="contactContainer">
       <div className="about-us">
         <h2 className="cursive-heading">Contact Us</h2>
-        <p>Email: contact@beanandivy.co.uk</p>
-        <p>Phone: +44 (0)20 7946 0958</p>
         <p>
-          Visit us at Bean & Ivy Coffee Shop, 45 Mayfair Lane, Mayfair, London,
-          W1K 7AA, United Kingdom
+          <strong>Email:</strong> contact@beanandivy.com
+        </p>
+        <p>
+          <strong>Phone:</strong> +44 (0)20 7946 0958
+        </p>
+        <p>
+          <strong>Visit us</strong>: Bean & Ivy Coffee Shop, 45 Mayfair Lane,
+          Mayfair, London, W1K 7AA, United Kingdom
         </p>
       </div>
     </div>
