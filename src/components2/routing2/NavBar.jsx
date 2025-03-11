d1f367de56c5731f29f2b51444fdbc455003f9c8
zcopycat <zvilsdorfa@outlook.com>
2025-03-11 23:58:24 +0000

    updated navbar styling for responsive design

--- src/components2/routing2/NavBar.jsx
+++ src/components2/routing2/NavBar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles2/Navbar.css"; // Navbar styling
+import logo from "../../images/bean_ivy_logo.jpg"; // ✅ added back as import (z)
 
 const Navbar = () => {
-  const logo = "/images/bean and ivy.avif"; // ✅ Correct public folder reference
+  // const logo = "../../images/bean_ivy_logo.jpg"; // 🥀 This code did not work in my vscode (zv)
 
   return (
     <nav className="navbar">
-      <h1 className="navbar-cafe-name">BEAN & IVY CAFÉ</h1>
+      {/* <h1 className="navbar-cafe-name">BEAN & IVY CAFÉ</h1> */}
+      <img
+        src={logo} // ✅ Fixed: Corrected path
+        alt="Outline of a coffee bean and ivy leaf with the words written underneath: Bean & Ivy"
+        className="navbar-logo"
+      />
       <ul className="nav-links">
         <li>
           <Link to="/">Home</Link>
@@ -16,18 +22,12 @@ const Navbar = () => {
           <Link to="/menu">Menu</Link>
         </li>
         <li>
-          <Link to="/about">About Us</Link>
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to="/contact">Contact Us</Link>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
-      {/* Logo Image */}
-      <img
-        src={logo} // ✅ Fixed: Corrected path
-        alt="Outline of a coffee bean and ivy leaf with the words written underneath: Bean & Ivy"
-        className="navbar-logo"
-      />
     </nav>
   );
 };
