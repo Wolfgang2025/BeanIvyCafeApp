12acb164771617be901d6277a4a575bfcf0cfb43
zcopycat <zvilsdorfa@outlook.com>
2025-03-12 19:36:49 +0000

    Updated components for the checkout and menu functionality - fixed version - now working

--- MenuPage.jsx
+++ MenuPage.jsx
@@ -1,41 +1,40 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-import CartSidebar from "../components2/CartSidebar";
-import { menuItems } from "../data2/menuData";
-import MenuItem from "../components2/MenuItem";
-import "../styles2/MenuPage.css";
+// import CartSidebar from "../components2/CartSidebar";
+// import { menuItems } from "../data2/menuData";
+// import MenuItem from "../components2/MenuItem";
+// import "../styles2/MenuPage.css";
 
+// const MenuPage = () => {
+//   const [cartItems, setCartItems] = useState([]);
+//   const [isCartOpen, setIsCartOpen] = useState(false);
 
-const MenuPage = () => {
-  const [cartItems, setCartItems] = useState([]);
-  const [isCartOpen, setIsCartOpen] = useState(false);
+//   const addToCart = (item) => {
+//     setCartItems([...cartItems, { ...item, quantity: 1 }]);
+//   };
 
-  const addToCart = (item) => {
-    setCartItems([...cartItems, { ...item, quantity: 1 }]);
-  };
+//   return (
+//     <div className="menu-page">
+//       <div className="menu-header">
+//         <h1>Our Menu</h1>
+//         <div className="cart-icon" onClick={() => setIsCartOpen(!isCartOpen)}>
+//           🛒 {cartItems.length > 0 && <span>{cartItems.length}</span>}
+//         </div>
+//       </div>
 
-  return (
-    <div className="menu-page">
-      <div className="menu-header">
-        <h1>Our Menu</h1>
-        <div className="cart-icon" onClick={() => setIsCartOpen(!isCartOpen)}>
-          🛒 {cartItems.length > 0 && <span>{cartItems.length}</span>}
-        </div>
-      </div>
+//       <div className="menu-grid">
+//         {menuItems.map((item) => (
+//           <MenuItem key={item.id} item={item} addToCart={addToCart} />
+//         ))}
+//       </div>
 
-      <div className="menu-grid">
-        {menuItems.map((item) => (
-          <MenuItem key={item.id} item={item} addToCart={addToCart} />
-        ))}
-      </div>
+//       <CartSidebar
+//         isOpen={isCartOpen}
+//         onClose={() => setIsCartOpen(false)}
+//         cartItems={cartItems}
+//       />
+//     </div>
+//   );
+// };
 
-      <CartSidebar
-        isOpen={isCartOpen}
-        onClose={() => setIsCartOpen(false)}
-        cartItems={cartItems}
-      />
-    </div>
-  );
-};
-
-export default MenuPage;
+// export default MenuPage;
--- src/pages2/MenuPage.jsx
+++ src/pages2/MenuPage.jsx
@@ -1,25 +1,58 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-import CartSidebar from "../components2/CartSidebar";
+// import CartSidebar from "../components2/CartSidebar";
+// import { menuItems } from "../data2/menuData";
+// import MenuItem from "../components2/MenuItem";
+// import "../styles2/MenuPage.css";
+
+// const MenuPage = () => {
+//   const [cartItems, setCartItems] = useState([]);
+//   const [isCartOpen, setIsCartOpen] = useState(false);
+
+//   const addToCart = (item) => {
+//     setCartItems([...cartItems, { ...item, quantity: 1 }]);
+//   };
+
+//   return (
+//     <div className="menu-page">
+//       <div className="menu-header">
+//         <h2>Our Menu</h2>
+//         <div className="cart-icon" onClick={() => setIsCartOpen(!isCartOpen)}>
+//           🛒 {cartItems.length > 0 && <span>{cartItems.length}</span>}
+//         </div>
+//       </div>
+
+//       <div className="menu-grid">
+//         {menuItems.map((item) => (
+//           <MenuItem key={item.id} item={item} addToCart={addToCart} />
+//         ))}
+//       </div>
+
+//       <CartSidebar
+//         isOpen={isCartOpen}
+//         onClose={() => setIsCartOpen(false)}
+//         cartItems={cartItems}
+//       />
+//     </div>
+//   );
+// };
+
+// export default MenuPage;
+
+import React from "react";
+import { Link } from "react-router-dom";
 import { menuItems } from "../data2/menuData";
 import MenuItem from "../components2/MenuItem";
 import "../styles2/MenuPage.css";
 
-const MenuPage = () => {
-  const [cartItems, setCartItems] = useState([]);
-  const [isCartOpen, setIsCartOpen] = useState(false);
-
-  const addToCart = (item) => {
-    setCartItems([...cartItems, { ...item, quantity: 1 }]);
-  };
-
+const MenuPage = ({ cartItems, addToCart }) => {
   return (
     <div className="menu-page">
       <div className="menu-header">
-        <h2>Our Menu</h2>
-        <div className="cart-icon" onClick={() => setIsCartOpen(!isCartOpen)}>
+        <h1 className="cursive-heading">Our Menu</h1>
+        <Link to="/checkout" className="cart-icon">
           🛒 {cartItems.length > 0 && <span>{cartItems.length}</span>}
-        </div>
+        </Link>
       </div>
 
       <div className="menu-grid">
@@ -27,12 +60,6 @@ const MenuPage = () => {
           <MenuItem key={item.id} item={item} addToCart={addToCart} />
         ))}
       </div>
-
-      <CartSidebar
-        isOpen={isCartOpen}
-        onClose={() => setIsCartOpen(false)}
-        cartItems={cartItems}
-      />
     </div>
   );
 };
