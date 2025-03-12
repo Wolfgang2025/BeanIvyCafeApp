34270b9cbfd272aa4f1ddd117dd332a68926e4e6
zcopycat <zvilsdorfa@outlook.com>
2025-03-12 00:03:33 +0000

    updated contact page styling to align with other pages

--- src/pages2/Contact.jsx
+++ src/pages2/Contact.jsx
@@ -1,24 +1,25 @@
 import React from "react";
+import "../styles2/Contact.css";
 
 const Contact = () => {
   return (
-    <div className="contactContainer">
-      <div className="about-us">
-        <h2 className="cursive-heading">✨📞 Contact Us ☕📍</h2>
-        <ul className="contact-list">
-          <li>
-            <strong>Email:</strong> contact@beanandivy.co.uk
-          </li>
-          <li>
-            <strong>Phone:</strong> +44 (0)20 7946 0958
-          </li>
-          <li>
-            <strong>Visit us</strong>: Bean & Ivy Café, 45 Mayfair Lane,
-            Mayfair, London, W1K 7AA, United Kingdom
-          </li>
-        </ul>
-      </div>
+    // <div className="contactContainer">
+    <div className="contact-box">
+      <h2 className="cursive-heading">✨📞 Contact Us ☕📍</h2>
+      <ul className="contact-list">
+        <li>
+          <strong>Email:</strong> contact@beanandivy.co.uk
+        </li>
+        <li>
+          <strong>Phone:</strong> +44 (0)20 7946 0958
+        </li>
+        <li>
+          <strong>Visit us</strong>: Bean & Ivy Café, 45 Mayfair Lane, Mayfair,
+          London, W1K 7AA, United Kingdom
+        </li>
+      </ul>
     </div>
+    // </div>
   );
 };
 
