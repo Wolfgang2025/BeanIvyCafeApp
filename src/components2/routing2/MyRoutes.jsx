4b01d3cdc31b7b79a947647f77cb388f65c5afe1
zcopycat <zvilsdorfa@outlook.com>
2025-03-12 19:37:50 +0000

    updated App and MyRoutes files

--- src/components2/routing2/MyRoutes.jsx
+++ src/components2/routing2/MyRoutes.jsx
@@ -1,50 +1,7 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from "../routing2/NavBar"; //The Navbar is rendered above the <Routes> component, so it appears on all pages.
-// import HomePage from "../../pages2/HomePage";
-// import MenuPage from "../../pages2/MenuPage";
-// import ProductPage from "../../pages2/ProductPage";
-// import CheckoutPage from "../../pages2/CheckoutPage"; // Adjust the import path as needed
-// import AboutUs from "../../pages2/AboutUs";
-// import Contact from "../../pages2/Contact";
-// import OrderingTCs from "../../pages2/OrderingTCs";
-// import PrivacyPolicy from "../../pages2/PrivacyPolicy";
-// import Footer from "./Footer";
-// import "../../styles2/styles.css";
-
-// const MyRoutes = () => {
-//   return (
-//     <>
-//       <Router>
-//         <div className="nav-routes-container">
-//           <div>
-//             <Navbar />
-//           </div>
-//           <Routes>
-//             <Route path="/" element={<HomePage />} />
-//             <Route path="/menu" element={<MenuPage />} />
-//             <Route path="/product/:id/" element={<ProductPage />} />
-//             <Route path="/checkout" element={<CheckoutPage />} />
-//             <Route path="/about" element={<AboutUs />} />
-//             <Route path="/contact" element={<Contact />} />
-//             <Route path="/orderingtcs" element={<OrderingTCs />} />
-//             <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-//           </Routes>
-//         </div>
-//         <div className="footer">
-//           <Footer />
-//         </div>
-//       </Router>
-//     </>
-//   );
-// };
-
-// export default MyRoutes;
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./NavBar";
 import HomePage from "../../pages2/HomePage";
 import MenuPage from "../../pages2/MenuPage";
-import ProductPage from "../../pages2/ProductPage";
 import CheckoutPage from "../../pages2/CheckoutPage";
 import AboutUs from "../../pages2/AboutUs";
 import Contact from "../../pages2/Contact";
@@ -67,7 +24,6 @@ const MyRoutes = ({ cartItems, addToCart, updateQuantity, removeItem }) => {
               path="/menu"
               element={<MenuPage cartItems={cartItems} addToCart={addToCart} />}
             />
-            <Route path="/product/:id/" element={<ProductPage />} />
             <Route
               path="/checkout"
               element={
